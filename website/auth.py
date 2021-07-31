5266054e714a0eea954c27b72a8bbc3af15df41d
jana prasanna <emailtojana27@gmail.com>
2021-07-31 14:02:41 +0530

    Blog APP using Flask

--- website/auth.py
+++ website/auth.py
@@ -1,5 +1,9 @@
 from flask import Blueprint, render_template, redirect, url_for, request, flash
 #this particular file "auth.py" is created to organise all types of authentication pages that the users sees
+from . import db
+from .dbmodels import User
+from flask_login import login_user, logout_user, login_required, current_user
+from werkzeug.security import generate_password_hash, check_password_hash
 
 auth = Blueprint("auth", __name__)
 
