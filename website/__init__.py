a106c2fbca0bd084e1d9f2ff7b903a0e394ca1dc
jana prasanna <emailtojana27@gmail.com>
2021-07-31 14:00:12 +0530

    Blog APP using Flask

--- website/__init__.py
+++ website/__init__.py
@@ -1,10 +1,23 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from os import path
+from flask_login import LoginManager
 
+db = SQLAlchemy()
+DB_NAME = "blogapp.db"
 
 def createapp():
+    #configuring Flask modules
     myapp = Flask(__name__)
     myapp.config["SECRET_KEY"] = "key_jana"
+    myapp.config['SECRET_KEY'] = "helloworld"
 
+    #configuring SQL
+    myapp.config['SQL_ALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
+    db.init_app(myapp)
+    create_database(myapp)
+
+    #importing blueprints
     from .views import views
     myapp.register_blueprint(views,url_prefix="/")
 
@@ -14,3 +27,10 @@ def createapp():
 
 
     return myapp
+
+
+
+def create_database(myapp):
+    if not path.exists("website/"+DB_NAME):
+        db.create_all(app=myapp)
+        print("Database created!")
