6467e10bbf2047d804d1d9d17513034febded259
jana prasanna <emailtojana27@gmail.com>
2021-08-01 21:27:49 +0530

    Blog APP using Flask

--- website/auth.py
+++ website/auth.py
@@ -23,7 +23,7 @@ def login():
                 flash('Password is incorrect.', category='error')
         else:
             flash('Email does not exist.', category='error')
-    return render_template('login.html')
+    return render_template('login.html',user=current_user)
 
 
 @auth.route('/signup',methods=["GET","POST"])
@@ -56,7 +56,7 @@ def sign_up():
             login_user(new_user, remember=True)
             flash('User created!')
             return redirect(url_for('views.home'))
-    return render_template('signup.html')
+    return render_template('signup.html',user=current_user)
 
 
 @auth.route('/logout')
--- website/views.py
+++ website/views.py
@@ -1,5 +1,5 @@
 from flask import Blueprint, render_template, url_for
-from flask_login import login_required, current_user
+from flask_login import login_required, current_user #current user method stores the info of recently logged in or currently logged in user in session
 #this particular file "views.py" is created to organise all types of view pages that the users sees
 views = Blueprint("views", __name__)
 
@@ -10,4 +10,4 @@ views = Blueprint("views", __name__)
 @views.route('/home')
 @login_required #only if there is a user logged in, he can access home page !
 def home():
-    return render_template('home.html')
+    return render_template('home.html',user=current_user)
