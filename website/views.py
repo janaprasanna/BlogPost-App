efb8f2d6b32e40816476acdc149dd375719f9cd0
jana prasanna <emailtojana27@gmail.com>
2021-08-01 21:14:18 +0530

    Blog APP using Flask

--- website/views.py
+++ website/views.py
@@ -1,4 +1,5 @@
 from flask import Blueprint, render_template, url_for
+from flask_login import login_required, current_user
 #this particular file "views.py" is created to organise all types of view pages that the users sees
 views = Blueprint("views", __name__)
 
@@ -7,5 +8,6 @@ views = Blueprint("views", __name__)
 
 @views.route('/')
 @views.route('/home')
+@login_required #only if there is a user logged in, he can access home page !
 def home():
     return render_template('home.html')
